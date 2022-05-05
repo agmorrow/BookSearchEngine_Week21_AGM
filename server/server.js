263818ec4d3278da8c033f0f2969bd6421b05dac
agmorrow <morrow.austin5@gmail.com>
2022-05-04 22:35:59 -0600

    server running

---
+++ server/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const mongoose = require("mongoose");
+
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+
+//import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//create new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
+//integrate our Apollo server with Express application as middleware
+server.applyMiddleware({app});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks' ,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+db.once('open', () => {
+  app.listen(process.env.PORT || PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+  });
+});
+
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
+});
